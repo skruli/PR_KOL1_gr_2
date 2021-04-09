9828b6cab008c64e9c7987d98312d64732f27435
skruli_ <grzesiek.789123@gmail.com>
2021-04-09 12:12:34 +0200

    kolokwium

---
+++ navbar.js
@@ -0,0 +1,3 @@
+let nav = '<nav><h1>Nawigacja</h1><a href="index.html">Home</a>' + '<a href="kontakt.html">Kontakt</a>'+'<a href="formularz.html">formularz</a></nav>';
+let navbarElem = document.querySelector(".nav-bar");
+navbarElem.innerHTML = nav;
---
+++ script.js
@@ -0,0 +1,7 @@
+function zmienKolor(){
+  document.querySelector(".box").style.backgroundColor = "yellow";
+}
+
+function zmienTlo(){
+  document.getElementById("body").style.backgroundColor = "#7D7D7D"
+}
